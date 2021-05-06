c010525a0b8b853a658ce05738b0ed2221625c86
Umeh Amaka <amakaumeh01@gmail.com>
2021-05-06 11:36:18 +0100

    set up project layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/home.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <a href="/">Eche's Place</a>
       </header>
+      <main>
+        <h1>Products</h1>
+      </main>
+      <footer>
+        <p>All rights reserved </p>
+      </footer>
     </div>
   );
 }
